b45a0e28f1e4f12e4242a0f61e49cc9b83a1e88b
melinozturk <MelinOzturk0707@gmail.com>
2025-11-07 15:55:41 +0000

    start

--- ParanthesesChecker.java
+++ ParanthesesChecker.java
@@ -1,1 +1,11 @@
+// Melin Ozturk
+// 11/7/2025
+// This program will determine if the input string is valid based on criteria on strings. 
 
+
+public class ParanthesesChecker {
+//  Pre-condition: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type.
+//  Post-condition: Given a String s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+}
+
+public static void main 
