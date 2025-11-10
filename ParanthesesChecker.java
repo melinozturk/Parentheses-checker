acd7d7377ac461e4604e29b19145351010184ffc
melinozturk <MelinOzturk0707@gmail.com>
2025-11-10 15:59:39 +0000

    lalalalililililolololo

--- ParanthesesChecker.java
+++ ParanthesesChecker.java
@@ -2,15 +2,63 @@
 // 11/7/2025
 // This program will determine if the input string is valid based on criteria on strings. 
 
+import java.util.*;
 
 public class ParanthesesChecker {
-//  Pre-condition: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type.
-//  Post-condition: Given a String s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//  Pre-condition: User can only input  '(', ')', '{', '}', '[' and ']'. 
+//  Post-condition: Open brackets must be closed by the same type of brackets. Open brackets must be closed in the correct order. Every close bracket has a corresponding open bracket of the same type.
+    
+/**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String parantheses;
+
+        // Enter parantheses and press Enter
+        System.out.println("Input parantheses");
+        parantheses = scanner.nextLine();{
+
+        System.out.println(); }
+
+
+        if (parantheses.equals("(){}[]"));{
+        System.out.println("valid");
+        }
+
+        if (parantheses.equals("(([]){})"));{
+        System.out.println("valid");
+        }
+
+        if (parantheses.equals("([{({})}])")); {
+        System.out.println("valid");
+        }
+        
+
+        if (parantheses.equals("(]"));{
+            System.out.println("invalid");
+        }
+        
+        if (parantheses.equals("{}[())()(]"));{
+        System.out.println("invalid");
+        }
+
+        if (parantheses.equals("(){}{"));{
+        System.out.println("invalid");
+        }
+        
+    }
 }
 
-public static void main (String [] args)
-int i = 1
-while (int i = )
 
-
-if 
+//System.out.println(isValid("(){}[]")); //true.
+//System.out.println(isValid("(]")); //false.
+//System.out.println(isValid("([{({})}])")); //true .
+//System.out.println(isValid("(([]){})")); //true .
+//System.out.println(isValid("{}[())()(]")); //false .
+//System.out.println(isValid("(){}{")); //false  .
+//---------------------------------
+//Tricky!:
+//System.out.println(isValid("([)]")); //false
+//System.out.println(isValid("([{}([)])]")); //false
